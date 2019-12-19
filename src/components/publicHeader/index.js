d4cab8a944846147cae2a5c51746e930a190532d
张晴晴 <zhangqingqing@zhangqingqingdeMacBook-Pro.local>
2019-12-19 08:16:33 +0800

    首页动画

--- src/components/publicHeader/index.js
+++ src/components/publicHeader/index.js
@@ -5,6 +5,7 @@ import styles from "./index.less";
 import {NavBar} from 'antd-mobile';
 import {withRouter} from "react-router-dom";
 import classNames from "classnames";
+import { Transition } from 'react-transition-group';
 import {Link} from "react-router-dom"
 @withRouter
 class PublicHeader extends  Component{
@@ -14,32 +15,21 @@ class PublicHeader extends  Component{
             open: true,
         }
     }
-    onOpenChange = (...args) => {
-        this.setState({ open: !this.state.open });
-      }
-    componentDidMount(){
-       console.log(123,this.props.history)
-    }
     handleClose=()=>{
         this.setState({open:!this.state.open})
     }
     render(){
-        const sidebar =(
-            <div className={styles.list}>
-                <Link to="/">
-                    <span>首页</span>
-                    <span class="iconfont icon-icon1"></span>
-                </Link>
-                <Link to="/balance">
-                    <span>提现</span>
-                    <span class="iconfont icon-icon1"></span>
-                </Link>
-                <Link to="/helpcenter">
-                    <span>帮助中心</span>
-                    <span class="iconfont icon-icon1"></span>
-                </Link>
-            </div>  
-        )
+        const duration = 300;
+        const defaultStyle = {
+        transition: `left ${duration}ms ease-in-out`,
+        left:'-100%'
+        }
+        const transitionStyles = {
+        entering: { left: 0},
+        entered:  { left: 0},
+        exiting:  { left: '-100%' },
+        exited:  { left: '-100%' },
+        };
         return <div className={styles.header}>
             <NavBar 
             className={styles.title}
@@ -47,17 +37,27 @@ class PublicHeader extends  Component{
             onLeftClick={this.handleClose}
             rightContent={<span onClick={()=>{this.props.history.push("/record")}} className="iconfont icon-dlsz"></span>}
             >{this.props.title}</NavBar>
-             
-           <Drawer
-        className={styles.drawer}
-        // style={{ height: document.documentElement.clientHeight }}
-        // contentStyle={{ color: '#A6A6A6', textAlign: 'center', paddingTop: 42}}
-        sidebar={sidebar}
-        open={this.state.open}
-        onOpenChange={this.onOpenChange}
-      >
-          {this.props.children}
-      </Drawer>
+             <Transition in={this.state.open} timeout={500}>
+                {state => (
+                    <div className={styles.list} style={{
+                        ...defaultStyle,
+                        ...transitionStyles[state]
+                      }}>
+                        <Link to="/">
+                            <span>首页</span>
+                            <span className="iconfont icon-icon1"></span>
+                        </Link>
+                        <Link to="/balance">
+                            <span>提现</span>
+                            <span className="iconfont icon-icon1"></span>
+                        </Link>
+                        <Link to="/helpcenter">
+                            <span>帮助中心</span>
+                            <span className="iconfont icon-icon1"></span>
+                        </Link>
+                    </div>
+                )}
+            </Transition>
         </div>  
     }
 }
